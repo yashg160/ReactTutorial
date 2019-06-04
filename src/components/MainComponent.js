7234e1998764de6c8b85dba98357c806e196b736
Yash Gupta <yashg160@gmail.com>
2019-06-04 16:36:29 +0530

    Router Component Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetails from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
@@ -13,7 +17,10 @@ class Main extends Component {
                 super(props);
                 this.state = {
                         dishes: DISHES,
-                        selectedDish: null
+                        selectedDish: null,
+                        comments: COMMENTS,
+                        promotions: PROMOTIONS,
+                        leaders: LEADERS
                 };
         }
 
@@ -24,7 +31,10 @@ class Main extends Component {
         render() {
                 const HomePage = () =>{
                         return(
-                                <Home/>
+                                <Home dish={this.state.dishes.filter((dish) => dish.featured=true)[0]}
+                                        promotion={this.state.promotions.filter((promo) => promo.featured=true)[0]}
+                                        leader={this.state.leaders.filter((lead) => lead.featured=true)[0]}
+                                />
                         )
                 }
                 console.log("The selected Dish: " + this.state.selectedDish);
@@ -34,6 +44,7 @@ class Main extends Component {
                                 <Switch>
                                         <Route path="/home" component={HomePage} />
                                         <Route exact path="/menu" component={() => < Menu dishes={this.state.dishes} />} />
+                                        <Route exact path="/contactus" component={Contact} />
                                         <Redirect to="/home"/>
                                 </Switch>
                                 {/*<Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
