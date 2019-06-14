40ff03775ea3fc321f3ec5b553a0e3cbeb73fec3
Yash Gupta <yashg160@gmail.com>
2019-06-14 18:06:18 +0530

    Combining Reducers

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, {Component} from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Input, Label, Row, Col } from 'reactstrap';
 import {Link} from 'react-router-dom';
+import {Control, LocalForm, Errors} from 'react-redux-form';
 
 
 function RenderComments({ comments }) {
@@ -16,7 +17,10 @@ function RenderComments({ comments }) {
                 });
 
                 return (
-                        commentsBlock
+                        <div>
+                                {commentsBlock}
+                                <CommentForm></CommentForm>
+                        </div>
                 );
         }
 }
@@ -37,6 +41,7 @@ function RenderDish({ dish }) {
 const DishDetails = (props) => {
         
         if (props.dish != null) {
+
                 return (
                         <div className="container">
                                 <div className="row">
@@ -60,6 +65,7 @@ const DishDetails = (props) => {
                                         </div>
                                 </div>
                         </div>
+                        
                 );
         }
         else {
@@ -70,5 +76,83 @@ const DishDetails = (props) => {
 }
         
 export default DishDetails;
-        
+    
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+export class CommentForm extends Component{
+        
+        constructor(props){
+                super(props);
+                this.state = {
+                        isModalOpen: false
+                };
+
+                this.toggleModal = this.toggleModal.bind(this);
+                this.handleSubmit = this.handleSubmit.bind(this);
+        }
+
+        toggleModal() {
+                this.setState({
+                        isModalOpen: !this.state.isModalOpen
+                });
+        }
+
+        handleSubmit(values){
+                alert("Rating: " + values.rating + "Name: " + values.author + "Feedback: " + values.comment);
+        }
+
+        render(){
+                return (
+            <div>
+                <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-pencil"/> Submit Comment
+                </Button>
+
+                <div className="row row-content">
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}> Submit comment</ModalHeader>
+                        <ModalBody>
+                            <div className="col-12 col-md-9">
+                                <LocalForm onSubmit={(values) => this.handleSubmit(values)} >
+                                    <Row className="form-group">
+                                        <Label htmlFor="rating" md={4}>Rating</Label>
+                                        <Col className="col">
+                                            <Control.select model=".rating" name="rating" className="form-control" >
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </Control.select>
+                                        </Col>
+                                    </Row>
+
+                                    <Row className="form-group">
+                                        <Label htmlFor="author" md={4}>Your name</Label>
+                                        <Col className="col">
+                                            <Control.text model=".author" id="author" name="author" placeholder="Author" className="form-control" validators={{ required, minLength: minLength(3), maxLength: maxLength(15) }} />
+                                            <Errors className="text-danger" model=".author" show="touched" messages={{ required: 'Required', minLength: 'Must be greater than 3 characters', maxLength: 'Must be 15 charaters or less' }} />
+                                        </Col>
+                                    </Row>
+
+                                    <Row className="form-group">
+                                        <Label htmlFor="feedback" md={4}>Your feedback</Label>
+                                        <Col className="col">
+                                            <Control.textarea model=".comment" id="comment" name="comment" rows="6" className="form-control" validators={{ required }} />
+                                            <Errors className="text-danger" model=".comment" show="touched" messages={{ required: 'Required' }} />
+                                        </Col>
+                                    </Row>
+
+                                    <Button type="submit" value="submit" color="primary">Submit</Button>
+                                </LocalForm>
+                            </div>
+                        </ModalBody>
+                    </Modal>
+                </div>
+            </div>
+        );
+        }
+}
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, initState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initState
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
     return store;
 }
